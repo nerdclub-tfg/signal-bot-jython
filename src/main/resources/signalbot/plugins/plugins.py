0e5afac761477411d5f8badfe4993a0afd9744a7
Turakar <turakar23@gmail.com>
2016-10-27 19:00:03 +0200

    Remove body not empty check as it is not necessary anymore

--- src/main/resources/signalbot/plugins/plugins.py
+++ src/main/resources/signalbot/plugins/plugins.py
@@ -1,10 +1,10 @@
 from de.nerdclubtfg.signalbot import SignalInterface as signal
 from signalbot.patternplugin import PatternPlugin
 
-class Echo(PatternPlugin):
+class Plugins(PatternPlugin):
     
     def __init__(self, enabled):
-        PatternPlugin.__init__(self, enabled, '^!echo .*')
+        PatternPlugin.__init__(self, enabled, '^!plugins .*')
     
     def onMessage(self, sender, message, group):
-        signal.sendMessage(sender, message.getBody().get()[6:])
+        
