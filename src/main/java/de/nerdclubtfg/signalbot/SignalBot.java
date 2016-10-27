33f22335a23c99441c0a5def18e651482c9b8e18
Turakar <turakar23@gmail.com>
2016-10-27 22:28:21 +0200

    Fix plugin discovery to work in jars

--- src/main/java/de/nerdclubtfg/signalbot/SignalBot.java
+++ src/main/java/de/nerdclubtfg/signalbot/SignalBot.java
@@ -1,18 +1,15 @@
 package de.nerdclubtfg.signalbot;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeoutException;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.python.util.PythonInterpreter;
@@ -148,7 +145,8 @@ public class SignalBot implements ConversationListener, SecurityExceptionListene
 	}
 
 	private void loadPlugins() throws IOException {
-		plugins = getResourceFiles("/signalbot/plugins").stream()
+		plugins = ResourceList.getResources(Pattern.compile(".*signalbot/plugins/.*\\.py")).stream()
+				.map(v -> v.split("/")).map(v -> v[v.length - 1])
 				.map(v -> v.substring(0, v.length() - 3))
 				.filter(v -> !v.equals("__init__"))
 				.collect(Collectors.toList());
@@ -195,21 +193,6 @@ public class SignalBot implements ConversationListener, SecurityExceptionListene
 		python.set("paramGroup", group);
 		python.exec("from signalbot import bot\n"
 				+ "bot.onMessage(paramSender, paramMessage, paramGroup)");
-	}
-
-	private List<String> getResourceFiles(String path) throws IOException {
-		List<String> filenames = new ArrayList<>();
-
-		try (InputStream in = SignalBot.class.getResourceAsStream(path);
-				BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-			String resource;
-
-			while ((resource = br.readLine()) != null) {
-				filenames.add(resource);
-			}
-		}
-
-		return filenames;
 	}
 
 	private String firstUpperCase(String s) {
