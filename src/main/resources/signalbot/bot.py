fb52e4df79e983fb09a8b41a5e85cb612be503d6
Turakar <turakar23@gmail.com>
2016-10-29 12:25:34 +0200

    Add logging to bot.py

--- src/main/resources/signalbot/bot.py
+++ src/main/resources/signalbot/bot.py
@@ -1,11 +1,24 @@
 from de.nerdclubtfg.signalbot import SignalInterface as signal
+from datetime import datetime
 
 plugins = []
 
 def onMessage(sender, message, group):
+    called = []
     for plugin in plugins:
-        if plugin.enabled and plugin.accepts(sender, message, group):
-            try:
+        try:
+            if plugin.enabled and plugin.accepts(sender, message, group):
+                called.append(type(plugin).__name__)
                 plugin.onMessage(sender, message, group)
-            except Exception as e:
-                signal.sendMessage(sender, 'Error: %s' % type(e).__name__)
+        except Exception as e:
+            signal.sendMessage(sender, 'Error: %s' % type(e).__name__)
+    
+    print('[%s] %s%s: \"%s\" forwarded to %s' % (
+                        timestampToString(message.getTimestamp()),
+                        sender.getNumber(), 
+                        ('in group %s' % group.getName()) if group != None else '', 
+                        message.getBody().get() if message.getBody().isPresent() else 'no body', 
+                        called))
+
+def timestampToString(timestamp):
+    return datetime.fromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
