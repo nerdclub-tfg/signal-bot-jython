c1cf40deea4d7ae266ec2f0c73e8c267a43e320d
Turakar <turakar23@gmail.com>
2016-10-29 11:58:13 +0200

    Make bot more error robust

--- src/main/resources/signalbot/bot.py
+++ src/main/resources/signalbot/bot.py
@@ -1,8 +1,11 @@
-from signalbot.plugin import Plugin
+from de.nerdclubtfg.signalbot import SignalInterface as signal
 
 plugins = []
 
 def onMessage(sender, message, group):
     for plugin in plugins:
         if plugin.enabled and plugin.accepts(sender, message, group):
-            plugin.onMessage(sender, message, group)
+            try:
+                plugin.onMessage(sender, message, group)
+            except Exception as e:
+                signal.sendMessage(sender, 'Error: %s' % type(e).__name__)
